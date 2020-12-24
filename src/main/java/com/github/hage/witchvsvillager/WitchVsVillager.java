f50a0995785118c5b8e547f9820e6fe689ed948b
akon1248 <akon8321@gmail.com>
2020-12-24 17:50:33 +0900

    skin

--- src/main/java/com/github/hage/witchvsvillager/WitchVsVillager.java
+++ src/main/java/com/github/hage/witchvsvillager/WitchVsVillager.java
@@ -1,16 +1,26 @@
 package com.github.hage.witchvsvillager;
 
+import com.comphenix.protocol.ProtocolLibrary;
+import com.github.hage.witchvsvillager.util.skin.SkinListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class WitchVsVillager extends JavaPlugin {
 
+    private static WitchVsVillager instance;
+
     @Override
     public void onEnable() {
-
+        instance = this;
+        ProtocolLibrary.getProtocolManager().addPacketListener(new SkinListener());
     }
 
     @Override
     public void onDisable() {
-
+        ProtocolLibrary.getProtocolManager().removePacketListeners(this);
     }
+
+    public static WitchVsVillager inst() {
+        return instance;
+    }
+
 }
--- src/main/java/com/github/hage/witchvsvillager/util/MessageUtil.java
+++ src/main/java/com/github/hage/witchvsvillager/util/MessageUtil.java
@@ -5,6 +5,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
+import java.text.MessageFormat;
 import java.util.function.Predicate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -29,17 +30,6 @@ public class MessageUtil {
     }
 
     public static String format(String format, Object... args) {
-        String text = colored(format);
-        Matcher matcher = Pattern.compile("\\{\\d+}").matcher(format);
-        while (matcher.find()) {
-            String group = matcher.group();
-            int index = Integer.parseInt(group.substring(1, group.length() - 1));
-            if (args.length > index) {
-                text = text.replaceFirst(Pattern.quote(group), colored(String.valueOf(args[index]).replace("$", "\\$")));
-            } else {
-                text = text.replaceFirst(Pattern.quote(group), "null");
-            }
-        }
-        return text;
+        return MessageFormat.format(format, args);
     }
 }
